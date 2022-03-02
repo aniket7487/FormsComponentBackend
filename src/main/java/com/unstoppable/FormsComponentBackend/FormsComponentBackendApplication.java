beabf382f33772b011c8df70f4df3a082e8661ec
Aniket Mane <aniket.mane84@gmail.com>
2022-03-02 09:48:56 +0530

    initial commit

---
+++ src/main/java/com/unstoppable/FormsComponentBackend/FormsComponentBackendApplication.java
@@ -0,0 +1,13 @@
+package com.unstoppable.FormsComponentBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormsComponentBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormsComponentBackendApplication.class, args);
+	}
+
+}
